b1390a1459fbe68886f24c965f7e84d981ebb984
lordevito <lollo.devito@hotmail.com>
2023-11-22 20:18:57 +0100

    feat: snack1 completo

--- snack1/js/main.js
+++ snack1/js/main.js
@@ -1,1 +1,49 @@
 `use strict`;
+
+
+//Creiamo un array di oggetti con, ognuno, nome e peso della bici.
+//Creiamo anche un array vuoto che andremo a riempire solo con il peso delle bici
+
+const pesi = [];
+const bici = [
+    
+    {
+        nome: 'Bici1',
+        peso: '24'
+    },
+    {
+        nome: 'Bici2',
+        peso: '17'
+    },
+    {
+        nome: 'Bici3',
+        peso: '14'
+    },
+    {
+        nome: 'Bici4',
+        peso: '18'
+    },
+    {
+        nome: 'Bici5',
+        peso: '19'
+    }
+];
+
+//Creiamo un ciclo forEach per prendere il dato del peso delle bici e spostarli nell'array vuoto
+
+bici.forEach((element, index) => {
+    pesi.push(element.peso);
+    const {nome, peso} = bici[index];
+});
+
+//Creiamo una variabile e usiamo il metodo math.min per prendere il valore più basso
+
+const biciLeggera = Math.min(...pesi);
+
+//Creiamo una variabile per agganciarci l'elemento nel dom e trmite
+//template literal lo stampiamo a schermo
+
+const list = document.getElementById(`list`);
+list.innerHTML += `La bici più leggera pesa ${biciLeggera}kg`;
+
+
