d1dfbd906061c21f74cc5457b710b081c5f25a08
lordevito <lollo.devito@hotmail.com>
2023-11-22 21:06:19 +0100

    feat: snack 2 completo

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ Creiamo anche un array vuoto che andremo a riempire solo con il peso delle bici.
 Creiamo un ciclo forEach per prendere il dato del peso delle bici e spostarli nell'array vuoto.
 Creiamo una variabile e usiamo il metodo math.min per prendere il valore più basso.
 Creiamo una variabile per agganciarci l'elemento nel dom e trmite template literal lo stampiamo a schermo.
+
+Secondo snack:
+
+Creiamo un array di oggetti di squadre di calcio.
+Creiamo un array vuoto dove inseriremo il risultato finale.
+Inizializziamo una funzione per generare numeri random.
+Creiamo un ciclo ForEach dove destrutturiamo per prendere nome e falli subiti agganciamo i numeri random a falli subiti e punti fatti, poi pushiamo nome e falli subiti.
+Creiamo un ciclo for e stampiamo i risultati in console.
--- snack2/js/main.js
+++ snack2/js/main.js
@@ -1,1 +1,61 @@
 `use strict`;
+
+//Creiamo un array di oggetti di squadre di calcio.
+
+const squadreCalcio = [
+    {
+        nome: 'Lazio',
+        puntiFatti: '0',
+        falliSubiti: '0'
+
+    },
+    {
+        nome: 'Valencia',
+        puntiFatti: '0',
+        falliSubiti: '0'
+    },
+    {
+        nome: 'Liverpool',
+        puntiFatti: '0',
+        falliSubiti: '0'
+    },
+    {
+        nome: 'Tottenham',
+        puntiFatti: '0',
+        falliSubiti: '0'
+    },
+    {
+        nome: 'Ajax',
+        puntiFatti: '0',
+        falliSubiti: '0'
+    }
+
+];
+
+//Creiamo un array vuoto dove inseriremo il risultato finale
+
+const risultatoFinale = [];
+
+//Inizializziamo una funzione per generare numeri random
+
+function numeriRandom(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+};
+
+//Creiamo un ciclo ForEach dove destrutturiamo per prendere nome e falli subiti
+//agganciamo i numeri random a falli subiti e punti fatti, poi pushiamo nome e 
+//falli subiti
+
+squadreCalcio.forEach((element, index) =>{
+    element.puntiFatti = numeriRandom(2, 100);
+    element.falliSubiti = numeriRandom(2, 100);
+    const {nome} = squadreCalcio[index];
+    const {falliSubiti} = squadreCalcio[index];
+    risultatoFinale.push({nome, falliSubiti})
+});
+
+//Creiamo un ciclo for e stampiamo i risultati in console
+
+for (let i = 0; i < risultatoFinale.length; i++) {
+    console.log(`Squadra: ${risultatoFinale[i].nome}, Falli subiti: ${risultatoFinale[i].falliSubiti}`);
+}
